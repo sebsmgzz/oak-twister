1492740735bbd2e9bd9b48120863d2d8fa00d27d
sebsmgzz <sebastian.mgzz@outlook.com>
2021-02-22 16:42:42 -0600

    chore: immutable column

--- src/database/Column.java
+++ src/database/Column.java
@@ -1,72 +1,43 @@
 package database;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Column {
 
     private String name;
     private boolean isPrimaryKey;
     private Type type;
-    private int size;
     private boolean allowNull;
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public boolean isPrimaryKey() {
         return isPrimaryKey;
-    }
-
-    public void setPrimaryKey(boolean primaryKey) {
-        isPrimaryKey = primaryKey;
     }
 
     public Type getType() {
         return type;
     }
 
-    public void setType(Type type) {
-        this.type = type;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
     public boolean isAllowNull() {
         return allowNull;
     }
 
-    public void setAllowNull(boolean allowNull) {
+    public Column(String name, Type type, boolean allowNull, boolean isPrimaryKey) {
+        this.name = name;
+        this.type = type;
         this.allowNull = allowNull;
+        this.isPrimaryKey = isPrimaryKey;
     }
 
     @Override
     public String toString() {
-        List<String> definition = new ArrayList<>();
-        definition.add(name);
-        String strType = type.toString();
-        if (type == Type.TEXT) {
-            strType += "(" + size + ")";
-        }
-        definition.add(strType);
-        if(isPrimaryKey) {
-            definition.add("PRIMARY KEY");
-        }
-        if(!allowNull) {
-            definition.add("NOT NULL");
-        }
-        return String.join(" ", definition);
+        StringBuilder builder = new StringBuilder();
+        builder.append(name).append(" ");
+        builder.append(type.toString()).append(" ");
+        builder.append(isPrimaryKey? "PRIMARY KEY " : "");
+        builder.append(allowNull? "" : "NOT NULL ");
+        return builder.toString();
     }
 
 }
