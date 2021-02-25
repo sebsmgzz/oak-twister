189f5d8683b8e5a1d8a1a83f82c4cfefa9f5c781
sebsmgzz <sebastian.mgzz@outlook.com>
2021-02-24 20:52:06 -0600

    chore: started db utils commands

---
+++ src/commands/ImportDatabase.java
@@ -0,0 +1,79 @@
+package commands;
+
+import models.Account;
+import models.Identity;
+import models.Password;
+import models.Platform;
+import models.utils.ModelManager;
+import models.utils.ModelSerializer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ImportDatabase {
+
+    private final static String CSV_PATH = "C:\\Users\\SMaronas\\Repos\\OakTwister\\data\\csv";
+    private final static String SQLITE_PATH = "C:\\Users\\SMaronas\\Repos\\OakTwister\\data\\db.sqlite";
+
+    private final HashMap<String, ModelManager> managersMap;
+
+    public ImportDatabase() {
+        this.managersMap = new HashMap<>();
+        this.managersMap.put("accounts", new ModelManager<>(Account.class));
+        this.managersMap.put("identities", new ModelManager<>(Identity.class));
+        this.managersMap.put("passwords", new ModelManager<>(Password.class));
+        this.managersMap.put("platforms", new ModelManager<>(Platform.class));
+    }
+
+    public void execute() {
+        dropFile();
+        createTables();
+    }
+
+    public void dropFile() {
+        File file = new File(SQLITE_PATH);
+        if(file.delete()) {
+            System.out.println(file.getName() + " deleted");
+        }
+        else {
+            System.out.println("Failed to delete at " + SQLITE_PATH);
+        }
+    }
+
+    public void createTables() {
+        for(String key : managersMap.keySet()) {
+            System.out.print("Creating " + key + "...");
+            boolean created = managersMap.get(key).createTable();
+            if(created) {
+                System.out.println("success");
+                fillTable(key);
+            } else {
+                System.out.println("failed");
+            }
+        }
+    }
+
+    public void fillTable(String key) {
+        ModelManager manager = managersMap.get(key);
+        System.out.print("Filling " + key + "...");
+        try {
+            getValues(key);
+            System.out.println("success");
+        } catch (Exception e) {
+            System.out.println("failed");
+        }
+    }
+
+    public HashMap<String, String> getValues(String key) throws FileNotFoundException {
+        File file = new File(CSV_PATH + "\\" + key + ".csv");
+        Scanner myReader = new Scanner(file);
+        if (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+        return null;
+    }
+
+}
